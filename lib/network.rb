d7882664df6c498b8090499341061aacf4ad8c3d
Becky Robran <rerobran@gmail.com>
2019-10-16 10:39:01 -0600

    Finishes payroll method

--- lib/network.rb
+++ lib/network.rb
@@ -5,15 +5,31 @@ class Network
     @name = name
     @shows = []
   end
+
   def add_show(shows)
     @shows << shows
   end
+
   def highest_paid_actor
+
     characters = @shows.map do |show|
          show.characters
     end.compact.flatten
+
     characters.max_by do |character|
         character.salary
     end.actor
+  end
+
+  def payroll
+    characters = @shows.map do |show|
+         show.characters
+    end.compact.flatten
+
+    characters.reduce({}) do |payroll, character|
+      payroll[character.actor] = character.salary
+      payroll
+    end
+
   end
 end
