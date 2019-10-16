80e268086b0d5b6e3fa734493b5cf9d4aef1e2bc
Becky Robran <rerobran@gmail.com>
2019-10-16 09:43:10 -0600

    Adds shows method

--- lib/network.rb
+++ lib/network.rb
@@ -1,8 +1,11 @@
 class Network
-  attr_reader :name
+  attr_reader :name, :shows
 
   def initialize(name)
     @name = name
     @shows = []
   end
+  def add_show(shows)
+    @shows << shows
+  end 
 end
