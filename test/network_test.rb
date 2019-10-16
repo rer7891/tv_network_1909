115c56134a3e802b2854f84dd57a1d862a0ec6e3
Becky Robran <rerobran@gmail.com>
2019-10-16 09:42:53 -0600

    Adds shows

--- test/network_test.rb
+++ test/network_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/character'
 require './lib/show'
+require './lib/network'
 
 class NetworkTest < Minitest::Test
 
@@ -10,6 +11,9 @@ class NetworkTest < Minitest::Test
      @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
      @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
      @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+     @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -19,5 +23,9 @@ class NetworkTest < Minitest::Test
   def test_it_starts_with_no_shows
     assert_equal [], @nbc.shows
   end
-
+  def test_it_can_add_shows
+     @nbc.add_show(@knight_rider)
+     @nbc.add_show(@parks_and_rec)    
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
 end
