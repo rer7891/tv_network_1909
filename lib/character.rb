50de6d358c1dd150f8258078a985aad178484e47
Becky Robran <rerobran@gmail.com>
2019-10-16 09:37:12 -0600

    Adds Character Test

---
+++ lib/character.rb
@@ -0,0 +1,9 @@
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(details)
+    @name = details[:name]
+    @actor = details[:actor]
+    @salary = details[:salary]
+  end
+end
---
+++ lib/show.rb
@@ -0,0 +1,12 @@
+class Show
+  attr_reader :name, :creator, :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+  def total_salary
+    @characters.reduce(0) {|sum, character| sum + character.salary }
+  end
+end
